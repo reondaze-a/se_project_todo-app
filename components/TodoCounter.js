7976488f5f8a5b6e2d86166e1468c1cefcb779a3
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2025-04-01 12:09:34 -0400

    Update TodoCounter.js
    fixed syntax and made a method private as it has no use being accessed from the outside

--- components/TodoCounter.js
+++ components/TodoCounter.js
@@ -1,23 +1,21 @@
 class TodoCounter {
-    // todos should be the array of initial todos
-    // selector is the selector for the counter text element
     constructor(todos, selector) {
       this._element = document.querySelector(selector);
       this._completed = todos.filter((todo) => todo.completed).length;
       this._total = todos.length;
       document.addEventListener("todoCompleted", (evt) => {
-        this.updateCompleted(evt.detail.completed);
+        this._updateCompleted(evt.detail.completed);
       });
       document.addEventListener("todoDeleted", (evt) => {
         if (evt.detail.completed) {
-          this.updateCompleted(false);
+          this._updateCompleted(false);
         }
         this.updateTotal(evt.detail.increment);
       });
       this._updateText();
     }
 
-    updateCompleted = (increment) => {
+    _updateCompleted(increment) {
         if (increment) {
             this._completed += 1;
         } else {
@@ -25,7 +23,7 @@ class TodoCounter {
         }
         this._updateText();
     };
-    updateTotal = (increment) => {
+    updateTotal(increment) {
         if (increment) {
             this._total += 1;
         } else {
